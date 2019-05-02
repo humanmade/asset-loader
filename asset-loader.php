be7ea64da77cba2af67e5d72a4d07efaea413dc8
K Adam White <kadamwhite@users.noreply.github.com>
2019-05-02 11:05:13 -0400

    Transfer repository to HM organization

--- asset-loader.php
+++ asset-loader.php
@@ -11,13 +11,13 @@
  *
  * @wordpress-plugin
  * Plugin Name: Asset Loader
- * Plugin URI:  https://github.com/kadamwhite/asset-loader
+ * Plugin URI:  https://github.com/humanmade/asset-loader
  * Description: Utilities to seamlessly consume Webpack-bundled assets in WordPress themes & plugins.
  * Version:     0.3.0
  * Author:      K Adam White
  * Author URI:  http://kadamwhite.com
  * License:     GPL-2.0+
- * License URI: https//github.com/kadamwhite/asset-loader/tree/master/LICENSE
+ * License URI: https//github.com/humanmade/asset-loader/tree/master/LICENSE
  */
 require_once( plugin_dir_path( __FILE__ ) . 'inc/manifest/namespace.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'inc/paths/namespace.php' );
